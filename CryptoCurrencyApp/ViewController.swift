78bc5dcec5c1b616b5a6f5ab0e03776437651a49
Gustavo Silva <silvagustavo@me.com>
2022-01-07 12:20:46 +0300

    Initial Commit

---
+++ CryptoCurrencyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCurrencyApp
+//
+//  Created by Andre g f n Da silva on 07.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
