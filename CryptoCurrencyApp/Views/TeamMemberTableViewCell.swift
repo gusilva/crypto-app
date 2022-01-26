242cc59fc5c0a4221eb67847962cf58f08c05dd0
Gustavo Silva <silvagustavo@me.com>
2022-01-26 14:47:20 +0300

    wip

---
+++ CryptoCurrencyApp/Views/TeamMemberTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TeamMemberTableViewCell.swift
+//  CryptoCurrencyApp
+//
+//  Created by Andre g f n Da silva on 21.01.2022.
+//
+
+import UIKit
+
+class TeamMemberTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
