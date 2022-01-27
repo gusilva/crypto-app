89f0b451001e526a1bb0d4b7cb757b6d4868710c
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-01-27 16:25:05 +0300

    refactor api fetch data

--- CryptoCurrencyApp/Services/CoinService.swift
+++ CryptoCurrencyApp/Services/CoinService.swift
@@ -12,42 +12,15 @@ class CoinService {
   private let baseUrl = "https://api.coinpaprika.com/v1/coins"
   
   func fetchCoinDetail(coinId: String, completion: @escaping (Result<CoinDetail, CoinServiceError>) -> Void) {
-    guard let url = URL(string: "\(baseUrl)/\(coinId)") else {
-      completion(.failure(.general(reason: NSLocalizedString("ERROR_INIT_URL", comment: "error init url"))))
-      return
-    }
-    
-    URLSession.shared.dataTask(with: url) { data, response, error in
-      if
-        let _ = error,
-        let response = response as? HTTPURLResponse,
-        (200...299).contains(response.statusCode) {
-        completion(.failure(.network(statusCode: response.statusCode)))
-        return
-      }
-      
-      guard let data = data else {
-        completion(.failure(.general(reason: NSLocalizedString("ERROR_PARSING_DATA", comment: "parsing error"))))
-        return
-      }
-      
-      do {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        let decodedData = try decoder.decode(CoinDetail.self, from: data)
-        
-        completion(.success(decodedData))
-      } catch let error {
-        print(error)
-        completion(.failure(.parsing))
-        return
-      }
-    }.resume()
+    fetchData(urlString: "\(baseUrl)/\(coinId)", completion: completion)
   }
   
   func fetchCoins(completion: @escaping (Result<[Coin], CoinServiceError>) -> Void) {
-    guard let url = URL(string: baseUrl) else {
+    fetchData(urlString: baseUrl, completion: completion)
+  }
+  
+  private func fetchData<T: Decodable>(urlString: String, completion: @escaping (Result<T, CoinServiceError>) -> (Void)) {
+    guard let url = URL(string: urlString) else {
       completion(.failure(.general(reason: NSLocalizedString("ERROR_INIT_URL", comment: "error init url"))))
       return
     }
@@ -70,12 +43,12 @@ class CoinService {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         
-        let decodedData = try decoder.decode([Coin].self, from: data)
+        let result = try decoder.decode(T.self, from: data)
+        completion(.success(result))
         
-        completion(.success(decodedData))
-      } catch {
+      } catch let error {
+        print(error)
         completion(.failure(.parsing))
-        return
       }
     }.resume()
   }
